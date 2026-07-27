2b7007b3df287fc03a7d8f87cd238025341e5837
enderson28 <92381550+enderson28@users.noreply.github.com>
2026-07-26 21:54:39 -0400

    Update button colors in bot instructions

--- bot.py
+++ bot.py
@@ -83,8 +83,8 @@ TEXTO_START = (
     "🚀 <b>¿Cómo empezar? Usa el menú interactivo de botones aquí abajo o escribe los comandos:</b>\n"
     "➡️ <code>/precio</code> o botón 🟢🔴 <b>P2P~USDT</b> — Muestra las tasas reales BCV, precios P2P.\n"
     "➡️ <code>/intervencion</code> o botón 📊<b>Intervención</b> — Desglose de bolívares requeridos para la compra de dólares oficiales.\n"
-    "➡️ <code>/bpay</code> o botón 🔸<b>BPay</b> 🔸— Guía paso a paso para cargar USD bancarios a Binance.\n"
-    "➡️ <code>/gpay</code> o botón 🔹<b>GPay</b> 🔹— Ruta alternativa para Deposito USD usando Google Pay.\n\n"
+    "➡️ <code>/bpay</code> o botón 🔶<b>BPay</b> 🔶— Guía paso a paso para cargar USD bancarios a Binance.\n"
+    "➡️ <code>/gpay</code> o botón 🔵<b>GPay</b> 🔵— Ruta alternativa para Deposito USD usando Google Pay.\n\n"
     "💡 <b>Nota:</b> <i>Si eres nuevo, lee con atención ~ Boton 👇🏽📜 Regla de Oro 📜. ¡Evita comprar costoso en el P2P!</i>"
 )
 

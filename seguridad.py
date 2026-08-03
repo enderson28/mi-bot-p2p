bad01bb5014102f3097542973d7159846542297a
enderson28 <92381550+enderson28@users.noreply.github.com>
2026-08-02 20:18:30 -0400

    Add new VIP admin to the list

--- seguridad.py
+++ seguridad.py
@@ -82,6 +82,7 @@ def es_administrador(bot, chat_id, user_id, user=None):
 
 # Lista de administradores VIP (convertidos a minúsculas)
 ADMINS_VIP = [ 
+    "5073264705",
     "@antonys4", 
     "@papitamaster", 
     "@bazoner", 

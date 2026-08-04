2a3eeda7588ee862579fb6207cedc02df038c201
enderson28 <92381550+enderson28@users.noreply.github.com>
2026-08-04 11:33:07 -0400

    Change authorized users in bot.py
    Updated authorized users for the bot command.

--- bot.py
+++ bot.py
@@ -36,7 +36,7 @@ CANAL_PRUEBA = "@COMUNIDV"       # Canal de prueba
 CANAL_CONGESTIONADO = "@COMUNIDADAS04" # Canal principal
 CANAL_ADMINS = -1003947562741 # Reemplaza con el @ de tu grupo de admins
 # USUARIOS AUTORIZADOS PARA EL COMANDO /bot
-USUARIOS_AUTORIZADOS = [5073264705, "@AntonyS4", "@papitamaster"]
+USUARIOS_AUTORIZADOS = [5073264705, 1676933074, 6299629267]
 # Creador Supremo (Tu ID numérico real)
 CREADOR_ID = 5073264705  # Reemplaza por tu ID numérico
 

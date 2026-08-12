19d41afe49e1b2ba824bd21a8904e89ee8b0ca4f
enderson28 <92381550+enderson28@users.noreply.github.com>
2026-08-11 21:25:24 -0400

    Update restricted access message in captcha.py

--- captcha.py
+++ captcha.py
@@ -103,7 +103,7 @@ def setup_verification_handlers(bot, target_channel_id=None, funcion_menu=None, 
             message.chat.id,
             "⚠️ <b>Acceso Restringido</b>\n\n"
             "Este bot es de uso exclusivo para nuestra comunidad.\n"
-            "Solicita tu ingreso a través del enlace oficial del canal.",
+            "Solicita tu ingreso a través del enlace oficial del canal o 👉🏼 aqui @COMUNIDV .",
             parse_mode="HTML"
         )
 

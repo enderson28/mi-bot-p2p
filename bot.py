87567f19df72115bb492c45effdecdc3dc369963
enderson28 <92381550+enderson28@users.noreply.github.com>
2026-08-15 16:55:39 -0400

    Update admin command message formatting

--- bot.py
+++ bot.py
@@ -848,7 +848,7 @@ def handle_invitacion_comando(message):
         # 3. Si no es autorizado (usuario normal u otro admin), desintegra el aviso en 5 segundos
         aviso = bot.send_message(
             message.chat.id,
-            f'⚠️ <b>Comando exclusivo de los administradores principales:</b> Ender {e('PROGRAMADOR', '🐱')} Sarita {e('ESTRELLA', '⭐')}\n'
+            f'{e('PROGRAMADOR', '🐱')} Enderson <b>Comando exclusivo de los administradores principales:</b>\n'
             f'✨ <a href="tg://user?id=5073264705">Enderson</a>\n'
             f'⭐ <a href="tg://user?id=791436853">Sarita</a>',
             parse_mode="HTML",

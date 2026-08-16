a1a72f469cc60516adc76933ca7a795bd4daa54e
enderson28 <92381550+enderson28@users.noreply.github.com>
2026-08-16 02:31:25 -0400

    Fix formatting for tasas and montos in calculadora
    Updated formatting for tasas and montos in response messages.

--- calculadora.py
+++ calculadora.py
@@ -104,7 +104,7 @@ def registrar_calculadora(bot, obtener_cache_func, obtener_teclado_func):
                 f"<blockquote>{e('CONSULTA1', '🗯️')} RESULTADO DE CÁLCULO {e('BCV', '👏')}</blockquote>\n\n"
                 f" Monto en {e('DINERO', '💵')} : $<code>{monto_usd:,.2f}</code>\n"
                 f"{e('BCV', '👏')} Tasa BCV oficial: {tasa_bcv:,.2f} Bs.\n"
-                f"<blockquote>{e('BALANZA', '⚖️')} Tasa + 0.5% : {tasa_con_intervencion:,.4f} Bs.</blockquote>\n\n"
+                f"<blockquote>{e('BALANZA', '⚖️')} Tasa + 0.5% : {tasa_con_intervencion:,.2f} Bs.</blockquote>\n\n"
                 f"{e('pago_movil', '😶‍🌫️')} Total a pagar en Bolívares:\n"
                 f"<blockquote>{e('FLECHA_DERECHA', '👏')} <code>{monto_bolivares:,.2f}</code> Bs.</blockquote>\n\n"
                 f"{e('CHINCHE', '📌')} Puedes seguir escribiendo montos en $ o cambiar de modo abajo.\n"
@@ -116,9 +116,9 @@ def registrar_calculadora(bot, obtener_cache_func, obtener_teclado_func):
             
             respuesta = (
                 f"<blockquote>{e('CONSULTA1', '🗯️')} RESULTADO DE CÁLCULO DIVISAS AL {e('BCV', '👏')}</blockquote>\n\n"
-                f"🇻🇪 Monto disponible en Bs: {monto_bolivares:,.2f} Bs.\n"
+                f"🇻🇪 Monto disponible en Bs: {monto_bolivares:,.2f}\n"
                 f"{e('BCV', '👏')} Tasa BCV oficial: {tasa_bcv:,.2f} Bs.\n"
-                f"<blockquote>{e('BALANZA', '⚖️')} Tasa + 0.5% : <code>{tasa_con_intervencion:,.4f}</code> Bs.</blockquote>\n\n"
+                f"<blockquote>{e('BALANZA', '⚖️')} Tasa + 0.5% : <code>{tasa_con_intervencion:,.2f}</code> Bs.</blockquote>\n\n"
                 f"Puedes comprar un total de:\n"
                 f"<blockquote>{e('FLECHA_DERECHA', '➡️')} <code>{monto_usd:,.2f}</code> {e('DINERO', '💵')}</blockquote>\n\n"
                 f"{e('CHINCHE', '📌')} Puedes seguir escribiendo montos en Bs o cambiar de modo abajo.\n"

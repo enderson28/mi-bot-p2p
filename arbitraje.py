1c8c7962d0cfcd8625ca630c32a931f0211bf473
enderson28 <92381550+enderson28@users.noreply.github.com>
2026-08-22 13:51:44 -0400

    Refactor BCV rate retrieval from cache
    Updated the way BCV rates are retrieved from cache to use existing values if not found.

--- arbitraje.py
+++ arbitraje.py
@@ -1,4 +1,4 @@
-import json
+uimport json
 import logging
 import requests
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
@@ -493,8 +493,8 @@ def generar_y_enviar_resultado(chat_id, user_id, tasa_p2p_venta, bot, redis_clie
     tasa_zinli_usada = data_user.get("tasa_zinli_usada", None)
     
     # 1. Extraer tasas de Redis asegurando floats limpios
-    tasa_bcv_actual = float(cache_data.get("bcv_tasa", 784.66))
-    tasa_bcv_anterior = float(cache_data.get("bcv_tasa_anterior", 779.95))
+    tasa_bcv_actual = float(cache_data.get("bcv_tasa", tasa_bcv_actual))
+    tasa_bcv_anterior = float(cache_data.get("bcv_tasa_anterior", tasa_bcv_anterior))
 
     # Leemos la clave exacta que utiliza el scraper en Redis
     fecha_cache = str(cache_data.get("bcv_fecha") or cache_data.get("fecha") or "")

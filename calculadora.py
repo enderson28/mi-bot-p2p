543a147dc413e169bc976312c6f2f4cb0414f933
enderson28 <92381550+enderson28@users.noreply.github.com>
2026-09-14 23:23:37 -0400

    Update Redis connection in VIP access check

--- calculadora.py
+++ calculadora.py
@@ -24,7 +24,7 @@ def registrar_calculadora(bot, obtener_cache_func, obtener_teclado_func):
             
         # --- CONTROL DE ACCESO VIP ---
         # r_client / r es tu conexión Redis pasada o importada
-        if not es_usuario_vip_activo(bot, message.from_user, obtener_cache_func()):
+        if not es_usuario_vip_activo(bot, message.from_user, r):
             responder_sin_acceso_vip(bot, message.chat.id)
             return
         # -----------------------------
